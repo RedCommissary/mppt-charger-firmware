952b69e641e7f61102ad7da9fb56e358935d3bdb
RedCommissary <hardware.sitara@gmail.com>
2021-04-11 23:40:23 +0300

    new: Class for save user parameters of device

---
+++ source/application/inc/UserParameters.hpp
@@ -0,0 +1,46 @@
+/********************************************************************************
+ * class        List with user settings for application                         *
+ *                                                                              *
+ * file         UserParameters.h                                                        *
+ * author       @RedCommissary                                                  *
+ * date         31.12.2020                                                      *
+ *                                                                              *
+ ********************************************************************************/
+
+#pragma once
+
+/********************************************************************************
+ * Include
+ ********************************************************************************/
+
+#include <stdint.h>
+
+/********************************************************************************
+ * Class UserParameters
+ ********************************************************************************/
+class UserParameters {
+public:
+    enum class TypeDevices {
+        MPPT_2420_HW_v1,        // HW revision PCB in the aluminum case (2020)
+        MPPT_2420_HW_v2,        // HW revision PCB with TO-220 transistor (2021)
+        MPPT_4840_HW_v1,        // HW revision 150V input 40A for commercial (2021)
+        MPPT_4860_HW_v1         // HW revision 200V input 60A for commercial (2020)
+    };
+
+    enum class TypeBattery {
+        SLA,                    // Serviced lead battery
+        AGM,                    // Maintenance-free lead battery
+        GEL,                    // Gel lead battery
+        Lithium,                // Li-ion cell 18650 or similar
+        LiPo,                   // LiPo battery
+        LiFePO,                 // LiFePO battery
+        LTO                     // Lithium-titanate battery
+    };
+
+    TypeDevices device;
+    bool displayConnect;
+
+    TypeBattery battery;
+    uint8_t numberOfCells;
+    float chargeCurrent;
+};
